659ccfddb659831d9045993394a07e8e4c8144b9
Arnaud de Turckheim <quarium@gmail.com>
2018-02-27 11:00:04 +0100

    upipe_timecode: add timecode generation pipe

---
+++ lib/upipe-freetype/upipe_timecode.c
@@ -0,0 +1,494 @@
+/*
+ * Copyright (C) 2018 OpenHeadend S.A.R.L.
+ *
+ * Authors: Arnaud de Turckheim
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining
+ * a copy of this software and associated documentation files (the
+ * "Software"), to deal in the Software without restriction, including
+ * without limitation the rights to use, copy, modify, merge, publish,
+ * distribute, sublicense, and/or sell copies of the Software, and to
+ * permit persons to whom the Software is furnished to do so, subject
+ * to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be
+ * included in all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+ * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+ * MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+ * IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY
+ * CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,
+ * TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
+ * SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
+
+/** @file
+ * @short Upipe module for timecode rendering
+ */
+
+#include <upipe/upipe.h>
+#include <upipe/upipe_helper_upipe.h>
+#include <upipe/upipe_helper_urefcount.h>
+#include <upipe/upipe_helper_urefcount_real.h>
+#include <upipe/upipe_helper_flow.h>
+#include <upipe/upipe_helper_flow_def.h>
+#include <upipe/upipe_helper_uprobe.h>
+#include <upipe/upipe_helper_inner.h>
+#include <upipe/upipe_helper_bin_input.h>
+#include <upipe/upipe_helper_bin_output.h>
+#include <upipe/upipe_helper_uclock.h>
+#include <upipe/uref_clock.h>
+#include <upipe/uref_pic_flow.h>
+#include <upipe/uref_pic.h>
+#include <upipe/uprobe_prefix.h>
+#include <upipe/uclock.h>
+
+#include <upipe-modules/uprobe_blit_prepare.h>
+#include <upipe-modules/upipe_blit.h>
+
+#include <upipe-freetype/upipe_freetype.h>
+#include <upipe-freetype/upipe_timecode.h>
+
+#include <time.h>
+
+/** @internal @This is the private structure of timecode pipe. */
+struct upipe_tc {
+    /** public structure */
+    struct upipe upipe;
+    /** external reference */
+    struct urefcount urefcount;
+    /** reference counter */
+    struct urefcount urefcount_real;
+    /** inner blit pipe probe */
+    struct uprobe blit_probe;
+    /** inner text probe */
+    struct uprobe text_probe;
+    /** inner freetype probe */
+    struct uprobe freetype_probe;
+    /** inner blit pipe */
+    struct upipe *blit;
+    /** list of input requests */
+    struct uchain input_requests;
+    /** list of output requests */
+    struct uchain output_requests;
+    /** flow format attrutes */
+    struct uref *flow_def_attr;
+    /** input flow format */
+    struct uref *flow_def_input;
+    /** output pipe */
+    struct upipe *output;
+    /** timecode text */
+    struct upipe *text;
+    /** inner freetype pipe */
+    struct upipe *freetype;
+    /** uclock */
+    struct uclock *uclock;
+    /** uclock request */
+    struct urequest uclock_request;
+    /** freetype font */
+    char *font;
+};
+
+/** @hidden */
+static int upipe_tc_check(struct upipe *upipe, struct uref *flow_format);
+
+UPIPE_HELPER_UPIPE(upipe_tc, upipe, UPIPE_TC_SIGNATURE);
+UPIPE_HELPER_FLOW(upipe_tc, NULL);
+UPIPE_HELPER_UREFCOUNT(upipe_tc, urefcount, upipe_tc_noref);
+UPIPE_HELPER_UREFCOUNT_REAL(upipe_tc, urefcount_real, upipe_tc_free);
+UPIPE_HELPER_FLOW_DEF(upipe_tc, flow_def_input, flow_def_attr);
+UPIPE_HELPER_UPROBE(upipe_tc, urefcount_real, blit_probe, NULL);
+UPIPE_HELPER_UPROBE(upipe_tc, urefcount_real, text_probe, NULL);
+UPIPE_HELPER_UPROBE(upipe_tc, urefcount_real, freetype_probe, NULL);
+UPIPE_HELPER_INNER(upipe_tc, blit);
+UPIPE_HELPER_INNER(upipe_tc, text);
+UPIPE_HELPER_INNER(upipe_tc, freetype);
+UPIPE_HELPER_BIN_INPUT(upipe_tc, blit, input_requests);
+UPIPE_HELPER_BIN_OUTPUT(upipe_tc, blit, output, output_requests);
+UPIPE_HELPER_UCLOCK(upipe_tc, uclock, uclock_request,
+                    upipe_tc_check,
+                    upipe_tc_register_bin_request,
+                    upipe_tc_unregister_bin_request);
+
+/** @internal @This checks the timecode flow format.
+ *
+ * @param upipe description structure of the pipe
+ * @param flow_format timecode flow format
+ * @return an error code
+ */
+static int upipe_tc_check_flow_format(struct upipe *upipe,
+                                      struct uref *flow_def)
+{
+    UBASE_RETURN(uref_pic_flow_get_hsize(flow_def, NULL));
+    UBASE_RETURN(uref_pic_flow_get_vsize(flow_def, NULL));
+    UBASE_RETURN(uref_pic_get_hposition(flow_def, NULL));
+    UBASE_RETURN(uref_pic_get_vposition(flow_def, NULL));
+    return UBASE_ERR_NONE;
+}
+
+/** @internal @This allocates a timecode pipe.
+ *
+ * @param mgr pointer to manager
+ * @param uprobe structure used to raise events
+ * @param signature signature of the pipe allocator
+ * @param args optional arguments
+ * @return an allocated pipe or NULL
+ */
+static struct upipe *upipe_tc_alloc(struct upipe_mgr *mgr,
+                                    struct uprobe *uprobe,
+                                    uint32_t signature,
+                                    va_list args)
+{
+    struct uref *flow_def;
+    struct upipe *upipe = upipe_tc_alloc_flow(mgr, uprobe, signature, args,
+                                              &flow_def);
+    if (unlikely(!upipe))
+        return NULL;
+
+    upipe_tc_init_urefcount(upipe);
+    upipe_tc_init_urefcount_real(upipe);
+    upipe_tc_init_flow_def(upipe);
+    upipe_tc_init_bin_input(upipe);
+    upipe_tc_init_bin_output(upipe);
+    upipe_tc_init_blit_probe(upipe);
+    upipe_tc_init_text_probe(upipe);
+    upipe_tc_init_freetype_probe(upipe);
+    upipe_tc_init_text(upipe);
+    upipe_tc_init_freetype(upipe);
+    upipe_tc_init_uclock(upipe);
+
+    struct upipe_tc *upipe_tc = upipe_tc_from_upipe(upipe);
+    upipe_tc->flow_def_attr = flow_def;
+    upipe_tc->font = NULL;
+
+    upipe_throw_ready(upipe);
+
+    if (unlikely(!ubase_check(upipe_tc_check_flow_format(upipe, flow_def)))) {
+        upipe_err(upipe, "invalid flow format");
+        upipe_release(upipe);
+        return NULL;
+    }
+
+    struct upipe_mgr *upipe_blit_mgr = upipe_blit_mgr_alloc();
+    if (unlikely(!upipe_blit_mgr)) {
+        upipe_err(upipe, "fail to allocate blit manager");
+        upipe_release(upipe);
+        return NULL;
+    }
+    struct upipe *blit = upipe_void_alloc(
+        upipe_blit_mgr,
+        uprobe_pfx_alloc(
+            uprobe_blit_prepare_alloc(
+                uprobe_use(upipe_tc_to_blit_probe(upipe_tc))),
+            UPROBE_LOG_VERBOSE, "blit"));
+    upipe_mgr_release(upipe_blit_mgr);
+    if (unlikely(!blit)) {
+        upipe_err(upipe, "fail to allocate blit pipe");
+        upipe_release(upipe);
+        return NULL;
+    }
+    upipe_attach_upump_mgr(blit);
+    upipe_tc_store_bin_output(upipe, upipe_use(blit));
+    upipe_tc_store_bin_input(upipe, blit);
+
+    struct upipe *text = upipe_void_alloc_sub(
+        blit,
+        uprobe_pfx_alloc(
+            uprobe_use(upipe_tc_to_text_probe(upipe_tc)),
+            UPROBE_LOG_VERBOSE, "text"));
+    if (unlikely(!text)) {
+        upipe_err(upipe, "fail to allocate text");
+        upipe_release(upipe);
+        return NULL;
+    }
+    upipe_tc_store_text(upipe, text);
+    if (unlikely(!ubase_check(upipe_blit_sub_set_alpha_threshold(text, 0xff)))) {
+        upipe_err(upipe, "fail to set alpha threshold");
+        upipe_release(upipe);
+        return NULL;
+    }
+    if (unlikely(!ubase_check(upipe_blit_sub_set_z_index(text, 2)))) {
+        upipe_err(upipe, "fail to set z index");
+        upipe_release(upipe);
+        return NULL;
+    }
+
+    return upipe;
+}
+
+/** @internal @This frees a timecode pipe.
+ *
+ * @param upipe description structure of the pipe
+ */
+static void upipe_tc_free(struct upipe *upipe)
+{
+    struct upipe_tc *upipe_tc = upipe_tc_from_upipe(upipe);
+
+    upipe_throw_dead(upipe);
+
+    free(upipe_tc->font);
+    upipe_tc_clean_flow_def(upipe);
+    upipe_tc_clean_uclock(upipe);
+    upipe_tc_clean_freetype(upipe);
+    upipe_tc_clean_text(upipe);
+    upipe_tc_clean_bin_output(upipe);
+    upipe_tc_clean_bin_input(upipe);
+    upipe_tc_clean_freetype_probe(upipe);
+    upipe_tc_clean_text_probe(upipe);
+    upipe_tc_clean_blit_probe(upipe);
+    upipe_tc_clean_urefcount_real(upipe);
+    upipe_tc_clean_urefcount(upipe);
+    upipe_tc_free_flow(upipe);
+}
+
+/** @internal @This is called when there is no more external reference.
+ *
+ * @param upipe description structure of the pipe
+ */
+static void upipe_tc_noref(struct upipe *upipe)
+{
+    upipe_tc_store_bin_output(upipe, NULL);
+    upipe_tc_store_bin_input(upipe, NULL);
+    upipe_tc_store_freetype(upipe, NULL);
+    upipe_tc_store_text(upipe, NULL);
+    upipe_tc_release_urefcount_real(upipe);
+}
+
+/** @internal @This handles input buffers.
+ *
+ * @param upipe description structure of the pipe
+ * @param uref input buffer to handle
+ * @param upump_p reference to pump that generated the buffer
+ */
+static void upipe_tc_input(struct upipe *upipe,
+                           struct uref *uref,
+                           struct upump **upump_p)
+{
+    struct upipe_tc *upipe_tc = upipe_tc_from_upipe(upipe);
+
+    uint64_t pts = 0;
+    int ret = uref_clock_get_pts_sys(uref, &pts);
+    if (unlikely(!ubase_check(ret)))
+        upipe_warn(upipe, "non dated uref");
+
+    unsigned h = 0, m = 0, s = 0, ms = 0;
+    uint64_t real = 0;
+    if (upipe_tc->uclock)
+        real = uclock_to_real(upipe_tc->uclock, pts);
+    else
+        upipe_warn(upipe, "no clock");
+
+    time_t i_now = real / UCLOCK_FREQ;
+    struct tm date;
+    localtime_r(&i_now, &date);
+    char buffer[strlen("00:00:00.000") + 1];
+    sprintf(buffer, "%02u:%02u:%02u.%03u",
+            date.tm_hour, date.tm_min, date.tm_sec,
+            (unsigned)((real % UCLOCK_FREQ) / (UCLOCK_FREQ / 1000)));
+    struct uref *ft_txt = uref_sibling_alloc_control(uref);
+    uref_attr_set_string(ft_txt, buffer, UDICT_TYPE_STRING, "text");
+    if (unlikely(!upipe_tc->freetype))
+        upipe_warn(upipe, "freetype pipe is not allocated");
+    else
+        upipe_input(upipe_tc->freetype, ft_txt, NULL);
+    upipe_tc_bin_input(upipe, uref, upump_p);
+}
+
+/** @internal @This checks the internal state of the timecode pipe.
+ *
+ * @param upipe description structure of the pipe
+ * @param flow_format provided flow format
+ * @return an error code
+ */
+static int upipe_tc_check(struct upipe *upipe, struct uref *flow_format)
+{
+    struct upipe_tc *upipe_tc = upipe_tc_from_upipe(upipe);
+
+    if (flow_format)
+        uref_free(flow_format);
+
+    if (unlikely(!upipe_tc->uclock)) {
+        upipe_tc_require_uclock(upipe);
+        return UBASE_ERR_NONE;
+    }
+
+    return UBASE_ERR_NONE;
+}
+
+/** @internal @This sets a timecode option.
+ *
+ * @param upipe description structure of the pipe
+ * @param name option name
+ * @param value option value to set
+ * @return an error code
+ */
+static int upipe_tc_set_option(struct upipe *upipe, const char *name,
+                               const char *value)
+{
+    struct upipe_tc *upipe_tc = upipe_tc_from_upipe(upipe);
+
+    if (!strcmp(name, "font")) {
+        struct upipe *freetype = upipe_tc->freetype;
+
+        if (unlikely(!freetype)) {
+            upipe_tc->font = value ? strdup(value) : NULL;
+            return UBASE_ERR_NONE;
+        };
+
+        struct uref *flow_def = upipe_tc->flow_def_attr;
+        if (unlikely(!flow_def))
+            return UBASE_ERR_INVALID;
+
+        uint64_t v = 0, h = 0;
+        UBASE_RETURN(uref_pic_flow_get_vsize(flow_def, &v));
+        UBASE_RETURN(uref_pic_flow_get_hsize(flow_def, &h));
+
+        UBASE_RETURN(upipe_set_option(freetype, name, value));
+
+        /* get rendered string size at 2048 */
+        uint64_t width, height;
+        UBASE_RETURN(upipe_freetype_set_pixel_size(freetype, 2048));
+        struct upipe_freetype_bbox bbox;
+        UBASE_RETURN(upipe_freetype_get_bbox(freetype, "0123456789.:", &bbox));
+        height = bbox.max.y - bbox.min.y;
+        UBASE_RETURN(upipe_freetype_get_width(freetype, "00:00:00.000", &width));
+        if (unlikely(!height || !width))
+            return UBASE_ERR_INVALID;
+
+        /* change pixel size to fit h and v */
+
+        unsigned hpx = (h * 2048 * 95) / (width * 100);
+        unsigned vpx = (v * 2048 * 7) / (height * 10);
+        unsigned px = hpx > vpx ? vpx : hpx;
+        UBASE_RETURN(upipe_freetype_set_pixel_size(freetype, px));
+        UBASE_RETURN(upipe_freetype_get_bbox(freetype, "0123456789.:", &bbox));
+        height = bbox.max.y - bbox.min.y;
+        UBASE_RETURN(upipe_freetype_get_width(freetype,
+                                              "00:00:00.000", &width));
+        UBASE_RETURN(upipe_freetype_set_baseline(
+                freetype, (h - width) / 2, ((v - height) / 2) - bbox.min.y));
+        return UBASE_ERR_NONE;
+    }
+    else
+        return UBASE_ERR_UNHANDLED;
+}
+
+/** @internal @This allocates the inner freetype pipe.
+ *
+ * @param upipe description structure of the pipe
+ * @param flow_def input flow def
+ * @return an error code
+ */
+static int upipe_tc_alloc_freetype(struct upipe *upipe,
+                                   struct uref *flow_def)
+{
+    struct upipe_tc *upipe_tc = upipe_tc_from_upipe(upipe);
+
+    struct upipe_mgr *upipe_freetype_mgr = upipe_freetype_mgr_alloc();
+    UBASE_ALLOC_RETURN(upipe_freetype_mgr);
+    struct upipe *freetype = upipe_flow_alloc(
+        upipe_freetype_mgr,
+        uprobe_pfx_alloc(
+            uprobe_use(upipe_tc_to_freetype_probe(upipe_tc)),
+            UPROBE_LOG_VERBOSE, "freetype"),
+        flow_def);
+    upipe_mgr_release(upipe_freetype_mgr);
+    UBASE_ALLOC_RETURN(freetype);
+    upipe_tc_store_freetype(upipe, freetype);
+    struct uref *flow_def_freetype = uref_sibling_alloc_control(flow_def);
+    UBASE_ALLOC_RETURN(flow_def_freetype);
+    int ret = uref_flow_set_def(flow_def_freetype, "void.text.");
+    if (unlikely(!ubase_check(ret))) {
+        upipe_err(upipe, "fail to set freetype flow def");
+        uref_free(flow_def_freetype);
+        return ret;
+    }
+    ret = upipe_set_flow_def(freetype, flow_def_freetype);
+    uref_free(flow_def_freetype);
+    UBASE_RETURN(ret);
+    UBASE_RETURN(upipe_set_output(freetype, upipe_tc->text));
+    UBASE_RETURN(upipe_tc_set_option(upipe, "font", upipe_tc->font));
+    return UBASE_ERR_NONE;
+}
+
+/** @internal @This sets the input flow definition.
+ *
+ * @param upipe description structure of the pipe
+ * @param flow_def input flow format
+ * @return an error code
+ */
+static int upipe_tc_set_flow_def(struct upipe *upipe, struct uref *flow_def)
+{
+    struct upipe_tc *upipe_tc = upipe_tc_from_upipe(upipe);
+    struct uref *flow_def_ft =
+        upipe_tc_store_flow_def_input(upipe, uref_dup(flow_def));
+    UBASE_RETURN(upipe_set_flow_def(upipe_tc->blit, flow_def));
+    ubase_assert(upipe_tc_alloc_freetype(upipe, flow_def_ft));
+    uref_free(flow_def_ft);
+    return UBASE_ERR_NONE;
+}
+
+/** @internal @This handles the control commands.
+ *
+ * @param upipe description structure of the pipe
+ * @param command control command to handle
+ * @param args optional arguments
+ * @return an error code
+ */
+static int upipe_tc_control_real(struct upipe *upipe,
+                                 int command, va_list args)
+{
+    switch (command) {
+        case UPIPE_SET_OPTION: {
+            const char *name = va_arg(args, const char *);
+            const char *value = va_arg(args, const char *);
+            return upipe_tc_set_option(upipe, name, value);
+        }
+        case UPIPE_GET_OUTPUT:
+        case UPIPE_SET_OUTPUT:
+        case UPIPE_BIN_GET_LAST_INNER:
+            return upipe_tc_control_bin_output(upipe, command, args);
+        case UPIPE_REGISTER_REQUEST:
+        case UPIPE_UNREGISTER_REQUEST:
+        case UPIPE_SET_FLOW_DEF: {
+            struct uref *flow_def = va_arg(args, struct uref *);
+            return upipe_tc_set_flow_def(upipe, flow_def);
+        }
+        case UPIPE_BIN_GET_FIRST_INNER:
+            return upipe_tc_control_bin_input(upipe, command, args);
+    }
+    return UBASE_ERR_UNHANDLED;
+}
+
+/** @internal @This handles the control commands and checks the internal state.
+ *
+ * @param upipe description structure of the pipe
+ * @param command control command to handle
+ * @param args optional arguments
+ * @return an error code
+ */
+static int upipe_tc_control(struct upipe *upipe,
+                            int command, va_list args)
+{
+    UBASE_RETURN(upipe_tc_control_real(upipe, command, args));
+    return upipe_tc_check(upipe, NULL);
+}
+
+/** @internal @This the static management structure for timecode pipes. */
+static struct upipe_mgr upipe_tc_mgr = {
+    .signature = UPIPE_TC_SIGNATURE,
+    .refcount = NULL,
+    .upipe_alloc = upipe_tc_alloc,
+    .upipe_input = upipe_tc_input,
+    .upipe_control = upipe_tc_control,
+};
+
+/** @This returns the managemenet structure for timecode pipes.
+ *
+ * @return a pointer to the timecode management structure
+ */
+struct upipe_mgr *upipe_tc_mgr_alloc(void)
+{
+    return &upipe_tc_mgr;
+}
