ab4656eaf1919a10a190d796fb6353b8725dde89
Arnaud de Turckheim <quarium@gmail.com>
2018-07-16 18:39:15 +0200

    uqueue: use eventfd counter

--- include/upipe/uqueue.h
+++ include/upipe/uqueue.h
@@ -46,8 +46,6 @@ extern "C" {
 
 /** @This is the implementation of a queue. */
 struct uqueue {
-    /** FIFO */
-    struct ufifo fifo;
     /** number of elements in the queue */
     uatomic_uint32_t counter;
     /** maximum number of elements in the queue */
@@ -56,6 +54,20 @@ struct uqueue {
     struct ueventfd event_push;
     /** ueventfd triggered when data can be popped */
     struct ueventfd event_pop;
+    /** ueventfd triggered when input data is received */
+    struct ueventfd event_in;
+    /** ueventfd triggered when output data is received */
+    struct ueventfd event_out;
+    /** current input cursor */
+    unsigned in;
+    /** current input space */
+    unsigned in_count;
+    /** current output cursor */
+    unsigned out;
+    /** current output items */
+    unsigned out_count;
+    /** extra element storage */
+    void **extra;
 };
 
 /** @This returns the required size of extra data space for uqueue.
@@ -63,7 +75,7 @@ struct uqueue {
  * @param length maximum number of elements in the queue
  * @return size in octets to allocate
  */
-#define uqueue_sizeof(length) ufifo_sizeof(length)
+#define uqueue_sizeof(length) (length * sizeof (void *))
 
 /** @This initializes a uqueue.
  *
@@ -76,16 +88,30 @@ struct uqueue {
 static inline bool uqueue_init(struct uqueue *uqueue, uint8_t length,
                                void *extra)
 {
+    uatomic_init(&uqueue->counter, 0);
     if (unlikely(!ueventfd_init(&uqueue->event_push, true)))
         return false;
     if (unlikely(!ueventfd_init(&uqueue->event_pop, false))) {
         ueventfd_clean(&uqueue->event_push);
         return false;
     }
-
-    ufifo_init(&uqueue->fifo, length, extra);
-    uatomic_init(&uqueue->counter, 0);
+    if (unlikely(!ueventfd_init(&uqueue->event_in, false))) {
+        ueventfd_clean(&uqueue->event_push);
+        ueventfd_clean(&uqueue->event_pop);
+        return false;
+    }
+    if (unlikely(!ueventfd_init(&uqueue->event_out, false))) {
+        ueventfd_clean(&uqueue->event_push);
+        ueventfd_clean(&uqueue->event_pop);
+        ueventfd_clean(&uqueue->event_in);
+        return false;
+    }
     uqueue->length = length;
+    uqueue->in = 0;
+    uqueue->out = 0;
+    uqueue->in_count = length;
+    uqueue->out_count = 0;
+    uqueue->extra = (void **)extra;
     return true;
 }
 
@@ -127,6 +153,26 @@ static inline struct upump *uqueue_upump_alloc_pop(struct uqueue *uqueue,
                                 refcount);
 }
 
+/** @This updates the number of elements removed from the queue if needed.
+ *
+ * @param uqueue pointer to a uqueue structure
+ * @return true if elements are ready to be added
+ */
+static inline bool uqueue_fetch_in(struct uqueue *uqueue)
+{
+    if (!uqueue->in_count) {
+        uint64_t count = 0;
+        if (unlikely(!ueventfd_read_value(&uqueue->event_out, &count)))
+            count = 0;
+        uqueue->in_count += count;
+        if (!uqueue->in_count) {
+            ueventfd_read(&uqueue->event_push);
+            return false;
+        }
+    }
+    return true;
+}
+
 /** @This pushes an element into the queue.
  *
  * @param uqueue pointer to a uqueue structure
@@ -135,20 +181,35 @@ static inline struct upump *uqueue_upump_alloc_pop(struct uqueue *uqueue,
  */
 static inline bool uqueue_push(struct uqueue *uqueue, void *element)
 {
-    if (unlikely(!ufifo_push(&uqueue->fifo, element))) {
-        /* signal that we are full */
-        ueventfd_read(&uqueue->event_push);
+    if (!uqueue_fetch_in(uqueue))
+        return false;
 
-        /* double-check */
-        if (likely(!ufifo_push(&uqueue->fifo, element)))
+    uqueue->extra[uqueue->in] = element;
+    uqueue->in = (uqueue->in + 1) % uqueue->length;
+    uqueue->in_count--;
+    ueventfd_write_value(&uqueue->event_in, 1);
+    ueventfd_write(&uqueue->event_pop);
+    uatomic_fetch_add(&uqueue->counter, 1);
+    return true;
+}
+
+/** @This updates the number of elements added to the queue if needed.
+ *
+ * @param uqueue pointer to a uqueue structure
+ * @return true if the elements are ready to be removed
+ */
+static inline bool uqueue_fetch_out(struct uqueue *uqueue)
+{
+    if (!uqueue->out_count) {
+        uint64_t count = 0;
+        if (unlikely(!ueventfd_read_value(&uqueue->event_in, &count)))
+            count = 0;
+        uqueue->out_count += count;
+        if (!uqueue->out_count) {
+            ueventfd_read(&uqueue->event_pop);
             return false;
-
-        /* signal that we're alright again */
-        ueventfd_write(&uqueue->event_push);
+        }
     }
-
-    if (unlikely(uatomic_fetch_add(&uqueue->counter, 1) == 0))
-        ueventfd_write(&uqueue->event_pop);
     return true;
 }
 
@@ -159,23 +220,45 @@ static inline bool uqueue_push(struct uqueue *uqueue, void *element)
  */
 static inline void *uqueue_pop_internal(struct uqueue *uqueue)
 {
-    void *element = ufifo_pop(&uqueue->fifo, void *);
-    if (unlikely(element == NULL)) {
-        /* signal that we starve */
-        ueventfd_read(&uqueue->event_pop);
+    if (!uqueue_fetch_out(uqueue))
+        return NULL;
 
-        /* double-check */
-        element = ufifo_pop(&uqueue->fifo, void *);
-        if (likely(element == NULL))
-            return NULL;
+    void *element = uqueue->extra[uqueue->out];
+    uqueue->out = (uqueue->out + 1) % uqueue->length;
+    uqueue->out_count--;
+    ueventfd_write_value(&uqueue->event_out, 1);
+    ueventfd_write(&uqueue->event_push);
+    uatomic_fetch_sub(&uqueue->counter, 1);
+    return element;
+}
 
-        /* signal that we're alright again */
-        ueventfd_write(&uqueue->event_pop);
+/** @This pops multiple elements from the queue.
+ *
+ * @param uqueue pointer to a uqueue structure
+ * @param size number of elements to pop
+ * @param elements filled with the popped elements, must be at least size long
+ * @return the number of popped elements, may be less then size
+ */
+static inline unsigned uqueue_pop_all(struct uqueue *uqueue,
+                                      unsigned size,
+                                      void **elements)
+{
+    if (!uqueue_fetch_out(uqueue))
+        return 0;
+
+    unsigned popped = size > uqueue->out_count ? uqueue->out_count : size;
+    for (unsigned i = 0; i < popped; i++) {
+        elements[i] = uqueue->extra[uqueue->out];
+        uqueue->out = (uqueue->out + 1) % uqueue->length;
+        uqueue->out_count--;
     }
 
-    if (unlikely(uatomic_fetch_sub(&uqueue->counter, 1) == uqueue->length))
+    if (popped) {
+        ueventfd_write_value(&uqueue->event_out, popped);
         ueventfd_write(&uqueue->event_push);
-    return element;
+        uatomic_fetch_sub(&uqueue->counter, popped);
+    }
+    return popped;
 }
 
 /** @This pops an element from the queue with type checking.
@@ -203,7 +286,8 @@ static inline unsigned int uqueue_length(struct uqueue *uqueue)
 static inline void uqueue_clean(struct uqueue *uqueue)
 {
     uatomic_clean(&uqueue->counter);
-    ufifo_clean(&uqueue->fifo);
+    ueventfd_clean(&uqueue->event_in);
+    ueventfd_clean(&uqueue->event_out);
     ueventfd_clean(&uqueue->event_push);
     ueventfd_clean(&uqueue->event_pop);
 }
